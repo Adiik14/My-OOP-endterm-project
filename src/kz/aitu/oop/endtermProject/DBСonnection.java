6ea627674c2a659ca18c0680b1149644c01230aa
Daniyar445 <m.daniyar.abd.@gmail.com>
2021-03-11 17:35:23 +0600

    postJDBC interface

---
+++ src/kz/aitu/oop/endtermProject/DBСonnection.java
@@ -0,0 +1,14 @@
+package kz.aitu.oop.endtermProject;
+
+import java.sql.Connection;
+
+public interface DBСonnection {
+
+    public Connection connect(String url, String user, String password);
+    public String execSQL (String sql, String [] params);
+
+    public void Stop();
+
+
+    //public void GetConnectionResult();
+}
