4d85afcee53e00beef17dff932c1fdccfb652112
Daniyar445 <m.daniyar.abd.@gmail.com>
2021-03-11 17:34:14 +0600

    Main class

--- src/kz/aitu/oop/endtermProject/Main.java
+++ src/kz/aitu/oop/endtermProject/Main.java
@@ -2,7 +2,13 @@ package kz.aitu.oop.endtermProject;
 
 public class Main {
     public static void main(java.lang.String[] args) {
+        DBСonnection dbc;
+        dbc = new PostgreSQL();
+        String url = "jdbc:postgresql://localhost:5432/JavaOOP_HW4";
+        String user = "postgres";
+        String password = "1291";
 
+        dbc.connect(url, user, password);
 
     }
 }
